f9f21699fefc26345ef5ecb2d62f54f11c50b5cd
cengiz94 <sonsair11@gmail.com>
2022-08-21 10:26:36 +0300

    Results
    interface names edited, results added

--- northwind/src/main/java/kodlamaio/northwind/api/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/ProductsController.java
@@ -4,26 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kodlamaio.northwind.business.abstracts.IProductService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductsController {
 	
-	private IProductService _productService;
+	private ProductService _productService;
 	
 	@Autowired
-	public ProductsController(IProductService _productService) {
+	public ProductsController(ProductService _productService) {
 		super();
 		this._productService = _productService;
 	}
 
 	@GetMapping("/getall")
-	public List<Product> getAll(){
+	public DataResult<List<Product>> getAll(){
 		return this._productService.getAll(); 
+	}
+	
+	@PostMapping("/add")
+	public Result Add(@RequestBody Product product) {
+		return this._productService.Add(product);
 	}
 }   
--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -5,25 +5,36 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kodlamaio.northwind.business.abstracts.IProductService;
-import kodlamaio.northwind.dataAccess.abstracts.IProductDao;
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @Service
-public class ProductManager implements IProductService{
+public class ProductManager implements ProductService{
 	
 	
-	private IProductDao _productDao;
+	private ProductDao _productDao;
 	
 	@Autowired
-	public ProductManager(IProductDao productDao) {
+	public ProductManager(ProductDao productDao) {
 		super();
 		this._productDao = productDao;
 	}
 
 	@Override
-	public List<Product> getAll() {
-		return this._productDao.findAll();
+	public DataResult<List<Product>>getAll(){
+		return new SuccessDataResult<List<Product>>(this._productDao.findAll());
+	}
+
+	@Override
+	public Result Add(Product product) {
+		this._productDao.save(product);
+		return new SuccessResult("Product Added.");
+				
 	}
 
 }
